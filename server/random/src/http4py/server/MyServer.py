1eb6a61f363bfbc9d8238dbf23881458371a1651
David Denton <daviddenton@users.noreply.github.com>
2025-10-24 01:12:06 +0100

    Adding some infra

---
+++ server/random/src/http4py/server/MyServer.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from http4py.core import Request, Response
+
+
+class MyServer:
+    def handle(self, request: Request) -> Response:
+        pass
---
+++ server/random/src/http4py/server/__init__.py
@@ -0,0 +1,1 @@
+from .MyServer import MyServer
