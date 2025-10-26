eea87cf95a835e8792dd80ce5e5d589303888c1f
David Denton <daviddenton@users.noreply.github.com>
2025-10-26 18:17:40 +0000

    Update examples, rework filter

--- website/data/src/filters.py
+++ website/data/src/filters.py
@@ -3,14 +3,13 @@ from dataclasses import dataclass
 from http4py.core import Filter, HttpHandler, Request, Response
 from http4py.core.method import POST
 
-
 # Filters are just middleware that decorate a handler
 @dataclass(frozen=True)
 class ReverseContent(Filter):
-    def __call__(self, next: HttpHandler) -> HttpHandler:
+    def __call__(self, fn: HttpHandler) -> HttpHandler:
         def decorated(request: Request) -> Response:
             reversed_body = request.body.text[::-1]
-            return next(request.body_(reversed_body))
+            return fn(request.body_(reversed_body))
 
         return decorated
 
